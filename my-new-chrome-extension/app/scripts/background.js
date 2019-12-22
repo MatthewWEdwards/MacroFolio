82af1eb433aa1b0427cfd9406cb239a41a1c29e5
MatthewWEdwards <mathew41796@gmail.com>
2019-12-22 01:04:19 -0600

    Activate app on every chrome page

--- my-new-chrome-extension/app/scripts/background.js
+++ my-new-chrome-extension/app/scripts/background.js
@@ -4,3 +4,12 @@ chrome.runtime.onInstalled.addListener(function (details) {
   console.log('previousVersion', details.previousVersion);
 });
 console.log('\'Allo \'Allo! Event Page');
+
+
+// Activate extension on all pages
+chrome.declarativeContent.onPageChanged.removeRules(undefined, function() {
+  chrome.declarativeContent.onPageChanged.addRules([{
+    conditions: [new chrome.declarativeContent.PageStateMatcher({})],
+        actions: [new chrome.declarativeContent.ShowPageAction()]
+  }]);
+});
