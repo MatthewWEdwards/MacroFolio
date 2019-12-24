2bb97b4e49d271ff25d1c4d00af2a5c2915b44a9
MatthewWEdwards <mathew41796@gmail.com>
2019-12-24 16:09:28 -0600

    Reintroduce computer icons

--- macrofolio/src/js/background.js
+++ macrofolio/src/js/background.js
@@ -1,3 +1,6 @@
+import '../img/128.png'
+import '../img/32.png'
+
 // SRC: https://stackoverflow.com/questions/11484910/resolve-host-to-ip-address-and-display-it-in-a-popup-using-a-chrome-extension/11487578#11487578
 
 var tabToHost = {};
