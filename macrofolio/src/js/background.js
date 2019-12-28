1f95444c808b186199d929df4dafb0f969c466ce
MatthewWEdwards <mathew41796@gmail.com>
2019-12-28 15:20:18 -0600

    Move google IP requests code to separate file. Add dir to .gitignore

--- macrofolio/src/js/background.js
+++ macrofolio/src/js/background.js
@@ -1,62 +1,9 @@
 import '../img/128.png'
 import '../img/32.png'
 
-// SRC: https://stackoverflow.com/questions/11484910/resolve-host-to-ip-address-and-display-it-in-a-popup-using-a-chrome-extension/11487578#11487578
+import hostToIP from './background.js'
+import tabToHost from './background.js'
 
-var tabToHost = {};
-var hostToIP = {};
-
-function processUrl(tabId, url) {
-    // Get the host part of the URL. 
-    var host = url; 
-    var req = 'https://dns.google.com/resolve?name=' + url
-
-    // Map tabId to host
-    tabToHost[tabId] = host ? host=host[1] : '';
-
-    if (host && !hostToIP[host]) { // Known host, unknown IP
-        hostToIP[host] = 'N/A';    // Set N/A, to prevent multiple requests
-        // Get IP from a host-to-IP web service
-        var x = new XMLHttpRequest();
-        x.open('GET', req);
-        x.onload = function() {
-            var result = JSON.parse(x.responseText);
-            console.log(result)
-            if (result && result.Answer){
-                // Lookup successful, save address
-                hostToIP[host] = result.Answer[0].data
-                console.log(result.Answer[0])
-                return
-             }
-         };
-         x.send();
-    }
-    return
+window.setInterval(()=>{
+    console.log(hostToIP)
 }
-function setPopupInfo(tabId) { // Notify all popups
-    chrome.extension.getViews({type:'popup'}).forEach(function(global) {
-        global.notify(tabId);
-    });
-}
-
-// Remove entry from tabToIp when the tab is closed.
-chrome.tabs.onRemoved.addListener(function(tabId) {
-    delete tabToHost[tabId];
-});
-// Add entries: Using method 1 ( `onUpdated` )
-chrome.tabs.onUpdated.addListener(function(tabId, changeInfo, tab) {
-    if (changeInfo.status === 'loading' && changeInfo.url) {
-        console.log(processUrl(tabId, tab.url)); // or changeInfo.url, does not matter
-    }
-});
-
-// Init: Get all windows and tabs, to fetch info for current hosts
-chrome.windows.getAll({populate: true}, function(windows) {
-    windows.forEach(function(win) {
-        if (win.type == 'normal' && win.tabs) {
-            for (var i=0; i<win.tabs.length; i++) {
-                processUrl(win.tabs[i].id, win.tabs[i].url);
-            }
-        }
-    });
-});
--- macrofolio/webpack.config.js
+++ macrofolio/webpack.config.js
@@ -23,7 +23,8 @@ var options = {
   entry: {
     popup: path.join(__dirname, "src", "js", "popup.js"),
     options: path.join(__dirname, "src", "js", "options.js"),
-    background: path.join(__dirname, "src", "js", "background.js")
+    background: path.join(__dirname, "src", "js", "background.js"),
+    ip: path.join(__dirname, "src", "js", "ip.js")
   },
   output: {
     path: path.join(__dirname, "build"),
