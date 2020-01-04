4af525413c7ccababd90aa327d4e136ffa6b09b4
MatthewWEdwards <mathew41796@gmail.com>
2020-01-03 23:53:24 -0600

    Continuously read page html and count links

--- macrofolio/src/js/content.js
+++ macrofolio/src/js/content.js
@@ -1,6 +1,8 @@
-// TODO: Send HTML again on page change
-window.onload = function() { 
+async function send_html(){
     let doc = document.documentElement.outerHTML
     chrome.runtime.sendMessage({"op": "html", "doc": doc})
 }
 
+window.onload = function() { 
+    setInterval(send_html, 1000)
+}
