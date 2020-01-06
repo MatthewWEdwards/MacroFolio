2b8871ea345305cb6d82da2979cbd91b249bfa16
MatthewWEdwards <mathew41796@gmail.com>
2020-01-05 22:14:04 -0600

    links() now returns array of domain names, i.e. it strips links of superfluous material

--- macrofolio/src/js/links.js
+++ macrofolio/src/js/links.js
@@ -1,5 +1,8 @@
-const url_match = /(?:ht|f)tps?:\/\/[-a-zA-Z0-9.]+\.[a-zA-Z]{2,3}(?:\/(?:[^"<=]|=)*)?/gi
+const url_match = /(?:ht|f)tps?:\/\/([-a-zA-Z0-9.]+\.[a-zA-Z]{2,3})(?:\/(?:[^"<=]|=)*)?/gi
+const domain_match = /(?:ht|f)tps?:\/\/([-a-zA-Z0-9.]+\.[a-zA-Z]{2,3})(?:\/(?:[^"<=]|=)*)?/i
 
 export function links(str){
-    return str.match(url_match)
+    let urls = str.match(url_match)
+    urls.forEach((url, idx, arr)=>{arr[idx] = domain_match.exec(url)[1]})
+    return urls
 }
