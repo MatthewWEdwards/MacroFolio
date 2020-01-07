308d44671f4dcf20709f24b16b187d0d44e5316c
MatthewWEdwards <mathew41796@gmail.com>
2020-01-06 18:27:31 -0600

    Move d3 funcs to map.js

---
+++ macrofolio/src/js/map.js
@@ -0,0 +1,40 @@
+import * as d3 from 'd3'
+import { geoPath } from 'd3-geo'
+import world_countries from './world-countries.json'
+
+// SVG utils
+export function setup_svg(id){
+    var svg = d3.select(id).append("svg")
+        .attr("viewBox", "0 0 " + (width * viewbox_ratio) + " " + (height * viewbox_ratio))
+        .attr("width", width)
+        .attr("height", height)
+    return svg
+}
+
+// World map consts
+const aspect_ratio = 2.25
+const viewbox_ratio = 1.5
+const height = 300
+const width = height * aspect_ratio
+const projection = d3.geoEqualEarth()
+
+// World map funcs
+export function draw_map(svg){
+    svg.append("path")
+        .datum(world_countries)
+        .attr("d", d3.geoPath(projection));
+}
+
+// Circle consts
+const circle_color = "#ff0000"
+const circle_radius = 1
+const circleGenerator = d3.geoCircle()
+circleGenerator.radius(circle_radius)
+
+// Circle funcs
+export function add_circle(svg){
+    svg.append("path")
+        .datum(circleGenerator())
+        .attr("fill", circle_color)
+        .attr("d", d3.geoPath(projection));
+}
--- macrofolio/src/js/popup.js
+++ macrofolio/src/js/popup.js
@@ -1,47 +1,8 @@
 import css from '../css/popup.css'
-import * as d3 from 'd3'
-import { geoPath } from 'd3-geo'
-import world_countries from './world-countries.json'
+import { setup_svg, draw_map, add_circle } from './map.js'
 
 // popup.html consts
 const map_id = "#d3_plot"
-
-// SVG utils
-function setup_svg(){
-    var svg = d3.select(map_id).append("svg")
-        .attr("viewBox", "0 0 " + (width * viewbox_ratio) + " " + (height * viewbox_ratio))
-        .attr("width", width)
-        .attr("height", height)
-    return svg
-}
-
-// World map consts
-const aspect_ratio = 2.25
-const viewbox_ratio = 1.5
-const height = 300
-const width = height * aspect_ratio
-const projection = d3.geoEqualEarth()
-
-// World map funcs
-function draw_map(svg){
-    svg.append("path")
-        .datum(world_countries)
-        .attr("d", d3.geoPath(projection));
-}
-
-// Circle consts
-const circle_color = "#ff0000"
-const circle_radius = 1
-const circleGenerator = d3.geoCircle()
-circleGenerator.radius(circle_radius)
-
-// Circle funcs
-function add_circle(svg, circle){
-    svg.append("path")
-        .datum(circle)
-        .attr("fill", circle_color)
-        .attr("d", d3.geoPath(projection));
-}
 
 // Display number of unique IPs on popup.html
 var el = document.getElementById('num')
@@ -50,6 +11,6 @@ chrome.storage.sync.get('num', (data)=>{
     el.innerHTML = data.num
 })
 
-var svg = setup_svg()
+var svg = setup_svg(map_id)
 draw_map(svg)
-add_circle(svg, circleGenerator())
+add_circle(svg)
